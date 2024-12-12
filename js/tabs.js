10ee68423df1a5aa48db3f8d8f3f29878bb4e3a3
mathsuckerasian <164144369+mathsuckerasian@users.noreply.github.com>
2024-12-12 13:43:31 +0300

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,8 @@
+var videoButton = document.getElementById("videoButton");
+
+
+videoButton.onclick = function() {
+    // el.src = "";
+    // el.style.fontSize ="56px";
+    videoButton.innerText = "hyhyhujujujuju";
+}
