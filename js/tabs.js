e6a6ab47a1e7e9aad1ee518f4e0a266fb8ea7c4e
mathsuckerasian <164144369+mathsuckerasian@users.noreply.github.com>
2024-12-19 19:06:45 +0300

    Delete js/tabs.js

--- js/tabs.js
+++
@@ -1,8 +0,0 @@
-var videoButton = document.getElementById("videoButton");
-
-
-videoButton.onclick = function() {
-    // el.src = "";
-    // el.style.fontSize ="56px";
-    videoButton.innerText = "hyhyhujujujuju";
-}
